ef9273a17ed1336c7524d6dcb95cd20dd65425e0
banga <banga@10.77.22.98>
2019-03-20 09:10:14 +0100

    New GUI

--- Social/src/gui/Panneau.java
+++ Social/src/gui/Panneau.java
@@ -1,38 +1,43 @@
 package gui;
 
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class Panneau extends JPanel {
-
-  private int posX = -50;
-  private int posY = -50;
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final int START_X = 20;
+	private static final int START_Y = 100;
   public void paintComponent(Graphics g) {
-    // On décide d'une couleur de fond pour notre rectangle
-    g.setColor(Color.white);
-    // On dessine celui-ci afin qu'il prenne tout la surface
-    g.fillRect(0, 0, this.getWidth(), this.getHeight());
-    // On redéfinit une couleur pour notre rond
-    g.setColor(Color.red);
-    // On le dessine aux coordonnées souhaitées
-    g.fillOval(posX, posY, 50, 50);
-  }
+	  super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		printPlayers(g2);
+		printLocation(g2);
+		setBackground(Color.WHITE);
 
-  public int getPosX() {
-    return posX;
   }
+  
+  private void printPlayers(Graphics2D g2) {
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/man.png"), START_X+40, START_Y, 60, 60, null);
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/user-1.png"), START_X-10, START_Y-50, 45, 45, null);
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/user.png"), START_X-10, START_Y+70, 45, 45, null);
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/user-20.png"), START_X+100, START_Y-40, 45, 45, null);
+	  
 
-  public void setPosX(int posX) {
-    this.posX = posX;
+	  
   }
-
-  public int getPosY() {
-    return posY;
+  private void printLocation(Graphics2D g2) {
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/house-4.png"), START_X+40, START_Y-100, 40, 40, null);
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/location.png"), START_X+250, START_Y-100, 40, 40, null);
+	  g2.drawImage(SimulationUtility.readImage("src/img_projet/briefcase.png"), START_X+450, START_Y-100, 40, 40, null);
+	  
+	  
+	  
   }
-
-  public void setPosY(int posY) {
-    this.posY = posY;
-  }
-}
+}   
